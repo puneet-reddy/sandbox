ec9fd8dd4725f0252197d4dbf91f25b7812d4838
puneet-reddy <puneet.reddy@gmail.com>
2019-04-15 10:13:35 -0400

    Almost reached internationalization

--- projects/microblog/app/__init__.py
+++ projects/microblog/app/__init__.py
@@ -9,10 +9,12 @@ import os
 from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask import Flask
 from config import Config
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -21,6 +23,8 @@ login = LoginManager(app)
 login.login_view = 'login'
 migrate = Migrate(app,db)
 mail = Mail(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 from app import routes, models, errors
 
